913dd03a2dee0f5a545c61fdc6e7ec693a857702
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-07-22 19:42:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz(int)
     "Fizz"
     elsif int % 5 == 0 
     "Buzz"
-  elsif int % 3 == 0 and int % 5 == 0 
+    elsif int % 3 == 0 and int % 5 == 0 
     "FizzBuzz"
-  else 
+    else 
     
   end
 end
