cdbdf3ac9e45b47a9b02737de72ac2aebb2e6715
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-07-22 19:55:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 int = 1 
 def fizzbuzz(int)
-  if int % 3 == 0 
-    "Fizz"
+  if int % 3 == 0 and int % 5 == 0 
+    "FizzBuzz"
     elsif int % 5 == 0 
     "Buzz"
-    elsif int % 3 == 0 and int % 5 == 0 
-    "FizzBuzz"
+    elsif int % 3 == 0  
+    "Fizz"
     else 
     
   end
